6daeb1c869f90eff04370ebc46ecd0c8984efb55
guangLess <bha.guang@gmail.com>
2015-10-28 09:17:37 -0400

    imageExpand

--- youGiveLoveABadName/FISViewController.m
+++ youGiveLoveABadName/FISViewController.m
@@ -9,25 +9,94 @@
 #import "FISViewController.h"
 
 @interface FISViewController ()
+
 @property (strong, nonatomic) IBOutlet UIImageView *bonJovi;
-- (IBAction)expand:(id)sender;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomSpaceCons;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topSpaceCons;
+@property (assign, nonatomic) BOOL toggle;
+
+-(IBAction)expand:(id)sender;
 
 @end
 
+
 @implementation FISViewController
 
-- (void)viewDidLoad
+
+-(void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.toggle = YES;
+
 }
 
-- (void)didReceiveMemoryWarning
+-(void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)expand:(id)sender {
+-(IBAction)expand:(id)sender{
+    
+    if (self.toggle) {
+        [self animationONe];
+    } else {
+        [self animationTwo];
+    }
+    self.toggle = !self.toggle;
+
 }
+
+-(void)animationTwo{
+    
+    
+    [UIView animateKeyframesWithDuration:1 delay:0 options:0 animations:^{
+        
+        [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.7 animations:^{
+            self.bottomSpaceCons.constant = 177;
+            self.topSpaceCons.constant = 197;
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.7 relativeDuration:0.25 animations:^{
+            
+            self.bonJovi.transform = CGAffineTransformMakeScale(1, 0.80);
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.9 relativeDuration:0.1 animations:^{
+            
+            self.bonJovi.transform = CGAffineTransformMakeScale(1, 1);
+        }];
+        
+        
+        [self.view layoutIfNeeded];
+    } completion:nil];
+    
+}
+
+
+-(void)animationONe{
+    
+    [UIView animateKeyframesWithDuration:1.0 delay:0 options:0 animations:^{
+        
+        [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.25 animations:^{
+
+            self.bonJovi.transform = CGAffineTransformMakeScale(1, 0.70);
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.27 relativeDuration:0.7 animations:^{
+            self.bottomSpaceCons.constant = 0;
+            self.topSpaceCons.constant = 0;
+            self.bonJovi.transform = CGAffineTransformMakeScale(1, 1.11);
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.78 relativeDuration:0.15 animations:^{
+            self.bottomSpaceCons.constant = 0;
+            self.topSpaceCons.constant = 0;
+            self.bonJovi.transform = CGAffineTransformMakeScale(1, 1);
+        }];
+        
+        [self.view layoutIfNeeded];
+        
+    } completion:nil];
+}
+
 @end
